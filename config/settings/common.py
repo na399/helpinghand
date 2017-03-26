5bc3096984d1bc815c4b365ebdd6d3918bba032b
Max Adulyanukosol <namax7@gmail.com>
2017-03-26 13:32:37 +0200

    login redirect

--- config/settings/common.py
+++ config/settings/common.py
@@ -263,7 +263,7 @@ if False:
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'cms'
+LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
