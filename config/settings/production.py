d7741caf7aa43a17544ff50f7032dd6308a91f04
Max Adulyanukosol <namax7@gmail.com>
2017-03-26 10:52:05 +0200

    aws

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,33 +73,33 @@ INSTALLED_APPS += ("gunicorn", )
 # ------------------------
 # See: http://django-storages.readthedocs.org/en/latest/index.html
 
-if False:
-    INSTALLED_APPS += (
-        'storages',
-    )
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-    AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
+INSTALLED_APPS += (
+    'storages',
+)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-    # AWS cache settings, don't change unless you know what you're doing:
-    AWS_EXPIRY = 60 * 60 * 24 * 7
+AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
+AWS_AUTO_CREATE_BUCKET = True
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
 
-    # TODO See: https://github.com/jschneier/django-storages/issues/47
-    # Revert the following and use str after the above-mentioned bug is fixed in
-    # either django-storage-redux or boto
-    AWS_HEADERS = {
-        'Cache-Control': six.b('max-age=%d, s-maxage=%d, must-revalidate' % (
-            AWS_EXPIRY, AWS_EXPIRY))
-    }
+# AWS cache settings, don't change unless you know what you're doing:
+AWS_EXPIRY = 60 * 60 * 24 * 7
 
-    # URL that handles the media served from MEDIA_ROOT, used for managing
-    # stored files.
-    MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+# TODO See: https://github.com/jschneier/django-storages/issues/47
+# Revert the following and use str after the above-mentioned bug is fixed in
+# either django-storage-redux or boto
+AWS_HEADERS = {
+    'Cache-Control': six.b('max-age=%d, s-maxage=%d, must-revalidate' % (
+        AWS_EXPIRY, AWS_EXPIRY))
+}
+
+# URL that handles the media served from MEDIA_ROOT, used for managing
+# stored files.
+MEDIA_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 
 # Static Assets
 # ------------------------
